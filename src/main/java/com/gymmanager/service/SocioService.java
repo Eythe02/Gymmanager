fa1f956eaeb2b21eacc00e141995aaa84fcdb6ab
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-06 12:12:00 +0100

    Importe las clases necesarias para usar repositorios y colecciones

---
+++ src/main/java/com/gymmanager/service/SocioService.java
@@ -0,0 +1,9 @@
+package com.gymmanager.service;
+
+import com.gymmanager.model.Socio;
+import com.gymmanager.repository.SocioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
