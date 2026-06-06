2d7fc9d3b19a4022f09d73a5e1cadc99ab5a3fb4
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-06 11:47:50 +0100

    Importe las anotaciones que son necesarias para la base de datos

---
+++ src/main/java/com/gymmanager/model/Socio.java
@@ -0,0 +1,10 @@
+package com.gymmanager.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
