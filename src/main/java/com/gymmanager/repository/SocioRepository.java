2be0a3581793697fd3a49ed5824e40e9262fbbdf
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-06 12:08:17 +0100

    Cree la interfaz con herencia de JpaRepository

--- src/main/java/com/gymmanager/repository/SocioRepository.java
+++ src/main/java/com/gymmanager/repository/SocioRepository.java
@@ -5,3 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+public interface SocioRepository extends JpaRepository<Socio, Long> {
+
+    
+}
