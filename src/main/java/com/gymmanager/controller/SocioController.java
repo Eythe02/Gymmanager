944ab342f439ec0a1378680bf6fe14ea482de22c
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-06 12:22:12 +0100

    Importe los repositorios y clases  necesarias para este codigo

---
+++ src/main/java/com/gymmanager/controller/SocioController.java
@@ -0,0 +1,10 @@
+package com.gymmanager.controller;
+
+import com.gymmanager.model.Socio;
+import com.gymmanager.service.SocioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
